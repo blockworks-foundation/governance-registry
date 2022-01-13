7cd0867e85f9c65835bd19ce4c75005d8fecb0d2
microwavedcola1 <microwavedcola@gmail.com>
2022-01-13 10:30:47 +0100

    uncomment
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- run-publish.sh
+++ run-publish.sh
@@ -4,5 +4,5 @@ set -e -o pipefail
 anchor build
 cp ./target/idl/voter_stake_registry.json src/voter_stake_registry.json
 cp ./target/types/voter_stake_registry.ts src/voter_stake_registry.ts
-#yarn clean && yarn build && cp package.json ./dist/ && yarn publish dist
+yarn clean && yarn build && cp package.json ./dist/ && yarn publish dist
 
