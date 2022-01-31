ac0eef6452d40e2d50f860ba967e749aa4d9104c
Christian Kamm <mail@ckamm.de>
2022-01-31 20:09:01 +0100

    finish upgrading spl-governance

--- programs/voter-stake-registry/src/governance.rs
+++ programs/voter-stake-registry/src/governance.rs
@@ -12,7 +12,7 @@ macro_rules! vote_weight_record {
                 let vwr: spl_governance_addin_api::voter_weight::VoterWeightRecord =
                     anchor_lang::AnchorDeserialize::deserialize(&mut data)
                         .map_err(|_| anchor_lang::__private::ErrorCode::AccountDidNotDeserialize)?;
-                if vwr.account_type != spl_governance_addin_api::voter_weight::VoterWeightRecord {
+                if !solana_program::program_pack::IsInitialized::is_initialized(&vwr) {
                     return Err(anchor_lang::__private::ErrorCode::AccountDidNotSerialize.into());
                 }
                 Ok(VoterWeightRecord(vwr))
